e258c88dd87ba07950f9e74c55292027b47948b3
emmanuellecorre <55747798+emmanuellecorre@users.noreply.github.com>
2019-09-24 15:54:55 +0200

    Add files via upload

---
+++ atelier1.R
@@ -0,0 +1,36 @@
+2 + 3
+8 - 12
+14 * 25
+-0.3
+-3/10
+2.35/12.2
+1-sqrt(2)
+sort(c(2,5,1,3))
+2*(3+2)
+x <- 2
+a=2
+2->b
+b
+tailles <- c(167, 192, 173, 174, 172, 167, 171, 185, 163, 170)
+tailles
+length(tailles)
+mean(tailles) #moyenne
+var(tailles) #variance
+sd(tailles) #écart-type
+summary(tailles) #résumé
+hist(tailles) #histogramme
+tailles.m<-tailles/100
+tailles.m
+tailles^2
+sin(tailles)
+poids <- c(86, 74, 83, 50, 78, 66, 66, 51, 50, 55)
+length(poids)
+mean(poids)
+var(poids)
+sd(poids)
+summary(poids)
+imc=poids/tailles.m^2
+imc
+donnees=data.frame(tailles,poids,imc)
+donnees
+
